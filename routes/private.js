411e3a19fbd1e64a5f5342628942bb79044f3fa5
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-11-14 22:05:32 +0100

    Authorizing route with Bearer access token completed

--- routes/private.js
+++ routes/private.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
-router.route("/").get();
+const { getPrivateData } = require('../controllers/private');
+const { protect } = require("../middleware/auth");
+router.route("/").get(protect, getPrivateData);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
+app.use("/api/private", require("./routes/private"));
 
 // Error handler (Should be last piece of middleware)
 app.use(errorHandler);
