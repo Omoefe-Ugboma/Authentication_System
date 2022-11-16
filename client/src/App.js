045e7bd1b7d39f5d9c4fa832bc45885b33a334a3
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-11-16 14:07:23 +0100

    Creating the route for all screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const App = () => {
   return (
-    <div className="App">
-      React App
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/register" component={RegisterScreen} />
+          <Route exact path="/forgotpassword" component={ForgotPasswordScreen} />
+          <Route exact path="/passwordreset/:resetToken" component={ResetPasswordScreen} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
