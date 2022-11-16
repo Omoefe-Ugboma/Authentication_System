0835b67f58cddd5969e16d9328c1057e0acfdd17
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-11-16 13:33:50 +0100

    forget password and password reset completed

--- models/User.js
+++ models/User.js
@@ -47,7 +47,7 @@ UserSchema.methods.getSignedToken = function () {
 };
 
 UserSchema.methods.getResetPasswordToken = function () {
-    const resetToken = cryto.randomBytes(20).toString("hex");
+    const resetToken = crypto.randomBytes(20).toString("hex");
 
     this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
 
